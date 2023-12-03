4d5faa34b807415f27ba8ce6827b5acc53639cd4
gfx687 <cuzmin.dmitry@gmail.com>
2023-12-03 22:47:56 +0400

    added missing assets

--- src/constants.py
+++ src/constants.py
@@ -6,8 +6,15 @@ CLASSES = {
 }
 
 OVERCLOCKS = {
-    'pgl_compact_rounds': ('Frame_Overclock_Balanced.png', 'Icon_Upgrade_Ammo.png'),
+    'warthog_cycle_overload': ('Frame_Overclock_Unstable.png', 'Icon_Upgrade_FireRate.png'),
     'stubby_em_refire_booster': ('Frame_Overclock_Balanced.png', 'Icon_Upgrade_FireRate.png'),
+    'stubby_well_oiled_machine': ('Frame_Overclock_Clean.png', 'Icon_Upgrade_FireRate.png'),
+    'pgl_compact_rounds': ('Frame_Overclock_Balanced.png', 'Icon_Upgrade_Ammo.png'),
+    'pgl_pack_rat': ('Frame_Overclock_Clean.png', 'Icon_Upgrade_Ammo.png'),
+    'pgl_hyperprop': ('Frame_Overclock_Unstable.png', 'Icon_Upgrade_ProjectileSpeed.png'),
+    'bc_inferno': ('Frame_Overclock_Unstable.png', 'Icon_Upgrade_Heat.png'),
+    'sd_pcc': ('Frame_Overclock_Unstable.png', 'Icon_Upgrade_Explosion.png'),
+
     'hurricane_pbm': ('Frame_Overclock_Balanced.png', 'Icon_Upgrade_BulletPenetration.png'),
     'coilgun_umc': ('Frame_Overclock_Clean.png', 'Icon_Upgrade_Duration_V2.png')
 }
@@ -16,6 +23,7 @@ DIFFICULTIES = [
     'Hazard 6x2',
     'Hazard 6x2EX',
     'Hazard 6x2p4',
+    '6x2 Diablo',
     'Hazard Lx2'
 ]
 
